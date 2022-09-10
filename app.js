26fafb4b64b1ea7c14d9591740b34b79529fc686
Shyam Sundar <ssjha360@gmail.com>
2022-09-10 14:40:40 +0530

    dob sum correction

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const dob = document.querySelector("#dob");
 const luckyNum = document.querySelector("#lucky-num");
 const checkBtn = document.querySelectorAll("button")[0];
 const messageEl = document.querySelector("#message");
+const sum = document.querySelector("#sum");
 
 const printMessage = (text) => {
   messageEl.innerHTML = text;
@@ -11,7 +12,11 @@ const checkClickHandler = () => {
   console.log(dob.value);
   console.log(luckyNum.value);
   if (dob.value && luckyNum.value) {
-    let dobSum = dob.value.split("-").reduce((a, b) => Number(a) + Number(b));
+    let dobSum = dob.value
+      .split("-")
+      .join("")
+      .split("")
+      .reduce((a, b) => Number(a) + Number(b));
     console.log(dobSum);
     console.log(parseInt(dobSum) % parseInt(luckyNum.value) == 0);
     if (parseInt(dobSum) % parseInt(luckyNum.value) == 0) {
